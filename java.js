66b5813c66dc3570c5abb2cec6eb32b35fad4bed
vibhanshutiwari <vibhanshtiwari1999@gmail.com>
2020-04-02 20:44:01 +0530

    sum of all function

--- java.js
+++ java.js
@@ -1,12 +1,17 @@
 
-const add = (x,y) => x+y;
-const subtract = (x,y) => x-y;
-const multiply = (x,y) => x*y;
-const devide = (x,y) => x%y; 
+const add = x => x+4;
+const subtract = x => x-5;
+const multiply = x => x*6;
+const triple = x => x*12; 
 
-const anwser = funs => funs(4,7);
-console.log(anwser(add));
-
-
+const anwser = [
+    add,
+    subtract,
+    multiply,
+    triple,
+];
+ var num = 231;
+ anwser.forEach(fun =>fun=(num));
+  console.log(num);
  
 
