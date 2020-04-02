ca632907bfb33cd9dd367264424681db8aa5589a
vibhanshutiwari <vibhanshtiwari1999@gmail.com>
2020-04-02 20:34:25 +0530

    solve all multiply function

--- java.js
+++ java.js
@@ -1,7 +1,12 @@
-const sayname = (name ,last) => {
-    console.log('first'+ name);
-    console.log('last' + last);
-}
-sayname('shubham','tiwari');
 
+const add = (x,y) => x+y;
+const subtract = (x,y) => x-y;
+const multiply = (x,y) => x*y;
+const devide = (x,y) => x%y; 
+
+const anwser = funs => funs(4,7);
+console.log(anwser(add));
+
+
+ 
 
