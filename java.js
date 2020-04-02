e68a904aea49cfcea69efa9d526f1ba447429dde
vibhanshutiwari <vibhanshtiwari1999@gmail.com>
2020-04-02 21:21:05 +0530

    merge object

--- java.js
+++ java.js
@@ -1,24 +1,17 @@
+const object = {
+    name:'himansh',
+    last:'tiwari',
+    town:'kaleniya',
+};
 
-const value = ({name,age,city}) => {
-    var _name = name;
-    var _age = age;
-    var _city = city;
+const object2 = {
+    type:'name',
+    personal:'matter',
+    station:'mankaur',
+};
 
-    return {
-        getName: () => _name,
-        getAge: () => _age,
-        getCity: () => _city,
-
-        setName:newName => _name =newName,
-    };
-}
- 
-const ans = value ({
-    name:'shubham',
-    age:21,
-    city:'delhi',
-});
-console.log(ans.getName());
-
-ans.setName('aman');
-console.log(ans.getName());
+const merge = {
+   ...object,
+   ...object2,
+};
+console.log(merge);
