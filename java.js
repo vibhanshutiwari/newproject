aa1fee559db7b846cadc25532a3425cef453967f
vibhanshutiwari <vibhanshtiwari1999@gmail.com>
2020-04-03 13:49:34 +0530

    Reduce function

--- java.js
+++ java.js
@@ -1,9 +1,6 @@
 
 const list = [11,22,33,44,55,66,77,33,66,32,34,65];
-const ans = (a,b) => {
-  if(a>b) return -1;
-  if(a<b) return 1;
-}
-
-const result = list.slice().sort(ans);
-console.log(result);
+const ans = list.reduce((acc,x) => {
+return acc + x;
+},0)
+console.log(ans);
