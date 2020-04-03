5067d2589eee864882850a9c65a0d0382b70fd40
vibhanshutiwari <vibhanshtiwari1999@gmail.com>
2020-04-03 11:04:30 +0530

    Filter

--- java.js
+++ java.js
@@ -1,5 +1,5 @@
 
 const list = [1,2,3,4,5,6,7,8,9,10];
-const finds = x => x*2;
-const container = list.map(finds);
+const finds = x => x % 2 ==0;
+const container = list.filter(finds);
 console.log(container);
