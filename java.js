41a326b9c7604552efde9efbb2bbfd5e57ed2f24
vibhanshutiwari <vibhanshtiwari1999@gmail.com>
2020-04-03 22:40:52 +0530

    find how many time one name in list

--- java.js
+++ java.js
@@ -1,17 +1,18 @@
-const maxnumber = (x,y) => {
-   if(x > y) {
-    return;
-}
-   console.log(x);
-   maxnumber(x + 1,2);
- };
- maxnumber(1,2);
 
-var factorial = function fac(num) {
-    if(num == 1){
-     return 1;
-    } 
-    return num * fac(num-1);
- };
- var newvalue = factorial(5);
-console.log(newvalue);
+const list = [
+    'shubham',
+    'aman',
+    'manish',
+    'puneet',
+    'shubham',
+    'puneet',
+    'shubham',
+];
+const newmain = show => {
+    return show.reduce((acc,x) => ({
+        ...acc,
+        [x] : acc[x] ? acc[x] +1 : 1,
+    }),{});
+};
+
+console.log(newmain(list));
