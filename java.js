4cb03d44488fe735d6e918234bce8689773332ca
vibhanshutiwari <vibhanshtiwari1999@gmail.com>
2020-04-03 13:36:51 +0530

    sorting function

--- java.js
+++ java.js
@@ -1,20 +1,9 @@
 
-const list = [
-  {
-  name:'shubham',
-  age:21,
-  jobs:'yet not',
-  nick:'shu',
-  personal:'everythings',
-  become:' softwere developer',
-  salary:20000,
-},
- {
-  name:'himansh',
-  age:24,
-  personal:'everythings',
-  become:' softwere developer',
-  salary:20000,
+const list = [11,22,33,44,55,66,77,33,66,32,34,65];
+const ans = (a,b) => {
+  if(a>b) return -1;
+  if(a<b) return 1;
 }
-];
-console.log(list.slice(1));
+
+const result = list.slice().sort(ans);
+console.log(result);
