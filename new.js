a4bd0060c9ca60ead43c27f07a2b69a7a40b7ea7
vibhanshutiwari <vibhanshtiwari1999@gmail.com>
2020-04-06 21:11:57 +0530

    operators

--- new.js
+++ new.js
@@ -2,3 +2,19 @@
 const num = 2;
 const dou = '4';
 console.log(+num + +dou);
+
+const nas = '5';
+const nai = '2';
+console.log(nas / nai);
+
+const mul = 4;
+const multi = 6;
+console.log(mul ** multi);
+ 
+const counter = 4;
+console.log(2 * + counter);
+
+for(a = 3,b = 4,c = 5 * 3; a < 20; a++) {
+    console.log(c);
+}
+
