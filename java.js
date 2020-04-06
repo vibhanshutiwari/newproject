2d593d11fbf76addb0c69288cd281a69a4367e38
vibhanshutiwari <vibhanshtiwari1999@gmail.com>
2020-04-06 11:23:08 +0530

    slice function

--- java.js
+++ java.js
@@ -1,6 +1,19 @@
 
-const strings = ['shubham','manish','rahul','manoj'];
-const ans = strings.join(' ');
+const text = [ 
+    {
+    name:'shubham',
+    vill:'kaleniya',
+},
+{
+    name:'manish',
+    vill:'karanpur',
+}];
+const ans = text.splice(0);
 console.log(ans);
 
+const letter = ['nidhi','aman','rohit','manoj','sameer','ashish dada'];
+const result = letter.splice(4);
+console.log(result);
 
+
+ 
