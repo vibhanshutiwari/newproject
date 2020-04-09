abe69dd56353f090b1f4b908be9ac4c3e59e8049
vibhanshutiwari <vibhanshtiwari1999@gmail.com>
2020-04-09 14:59:19 +0530

    Array function

--- new.js
+++ new.js
@@ -1,32 +1,52 @@
-const nobject = {
-  name:'manish',
-  relatiion:'bhai',
-  salary: 2000
-  
+
+// constructor method //
+const User = (name, lname) => {
+    this.name = name;
+    this.lname = lname;
+}
+User('Shubham');
+console.log(this.name);
+
+
+// Array delete and splice use //
+const list = ['shu', 'ht', 'gth', 'tt'];
+
+delete list[3];
+console.log(list.length);
+
+let store = list.slice(2);
+console.log(store);
+
+// string value add in object with helps of splice //
+const newiteam = ['name', 'lman', 'nborn'];
+  const newvalue = newiteam.splice(1, 0, 'how');
+    
+  console.log(newiteam);
+
+  // another object add in array in object //
+const oneobject = [{
+    rollno: 21,
+    class: 9,
+}];
+newobject = {
+    anotherclass: 10,
+    otherstudentno: 33,
 };
 
-nobject.salary =  +3000;
-console.log(nobject.salary);
+oneobject.splice(0, 0, newobject);
+   
 
-// toString or valueOf //
-const list  = {
-    name:'shubham',
-    pincode: 320031,
-    valueOf () {
-        return this.pincode;
-    }
-};
-console.log(list * 2);
+// sorting in array //
+const sorted = [22.44,11,55,1];
+ 
+ const hey = (a,b) => {
+     if(a > b) return 1;
+     if(a < b) return -1;
+ }
+   sorted.sort(hey);
+   console.log(sorted);
 
-
-// Arrow function have no 'this'. //
-const newlist = {
-    country: 'india',
-
-    sayhello () {
-        let hi = () => 
-            console.log(this.country);
-            hi();
-        }
-};
- newlist.sayhello();
+    let marks = sorted.reduce((acc,x) => {
+       acc + x;
+   },0);
+   console.log(marks);
