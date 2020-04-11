e1d1e4ebce34c997cc082301bc6d43fb6a87c1c9
vibhanshutiwari <vibhanshtiwari1999@gmail.com>
2020-04-11 22:04:10 +0530

    inline function

--- java.js
+++ java.js
@@ -1,5 +1,17 @@
- 
- let age = 21;
 
- let mess = (age < 7) ? 'hey' : (age < 44) ? 'yes' :'not ';
- console.log(mess);
+var num = 3221;
+
+ const list = {
+  student: 'rahul',
+  studentid: 33213,
+  studentclass: 'nine',
+  teacher: function() {
+      console.log(this.studentid);
+  }
+ };
+ list.teacher();
+
+
+
+ 
+    
