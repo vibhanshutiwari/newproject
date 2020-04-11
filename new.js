29e615094279202d10af5a58c2a69f75ea29a297
vibhanshutiwari <vibhanshtiwari1999@gmail.com>
2020-04-11 20:42:11 +0530

    foreach and map

--- new.js
+++ new.js
@@ -1,58 +1,20 @@
 
-const instructor = {
-    firstname: 'shubham',
-    lastname: 'tiwari',
-    village: 'gonda',
-    course: 'javascript',
-    social: {
-        email: 'vibh@',
-        facebook: 'shubham',
-        tittok: 'shu@13'
-    },
-    interest: 'playing',
+const value = [{
+    name:'manish',
+    street : 3,
+},
+  {
+      name: 'makal',
+      studentID: 093739,
+  }];
+      
+ let nee = value.map(inst => {
+      return inst.name;
+  });
+   //console.log(nee);
 
-};
-  const {firstname,lastname,village,course,social} = instructor;
-  //console.log(firstname,lastname,village,course,social.facebook);
+ var num = 'manish';
 
- 
-  const newdestructor = [
-      {
-        firstname: 'shubham',
-        lastname: 'tiwari',
-        village: 'gonda',
-        course: 'javascript',
-        social: {
-            email: 'vibh@',
-            facebook: 'shubham',
-            tittok: 'shu@13'
-        },
-        interest: 'playing',
-      },
-      {
-        firstname: 'manish',
-        lastname: 'pandey',
-        village: 'lucknow',
-        course: '.net',
-        social: {
-            email: 'namhu@',
-            facebook: 'aman',
-            tittok: 'manu@13'
-        },
-        interest: 'teaching',
-        subject: 'chemistry',
-      }
-  ];
-  
-  //way 2
- // var obj = newdestructor.map((inst, indx) => {
-   //   console.log(indx);
-    //return {fname : inst.firstname, lname: inst.lastname, facebook : inst.social.facebook};
- // });
-  //console.log(obj);
-
- newdestructor.map(inst => {
-    const {firstname: fname,lastname: lname,village: vill,social: scl} = inst;
-
-    console.log(fname,lname,vill,scl.facebook);
- });
+     value.forEach(fid => fid.name == num);
+     console.log(num);
+    
