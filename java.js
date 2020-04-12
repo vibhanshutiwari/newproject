259b08187f271f36b2c84b057e813963adf7c135
vibhanshutiwari <vibhanshtiwari1999@gmail.com>
2020-04-12 13:21:01 +0530

    Template Literal

--- java.js
+++ java.js
@@ -1,17 +1,30 @@
 
-var num = 3221;
+// object value find with helps template literal ..
 
- const list = {
-  student: 'rahul',
-  studentid: 33213,
-  studentclass: 'nine',
-  teacher: function() {
-      console.log(this.studentid);
-  }
- };
- list.teacher();
+const stringobj = {
+  name: 'mohan das',
+  course: 'javascript',
+  website: 'www.myhome.in',
+  social: 'facebook',
+};
 
+const newobj = `${stringobj.name},${stringobj.course},${stringobj.website},${stringobj.social}`;
+console.log(newobj);
 
+// passed the parameter with helps object key..
 
- 
-    
+const para = (name, ...num) => {
+   console.log('name',name);
+   console.log('second',num);
+}
+para('hello',`${stringobj.name},${stringobj.course},${stringobj.website},${stringobj.social}`);
+  
+
+// store string five line in one variable ...
+
+const write =  `On sunday,Manu is parents  got him a red raincoat.
+Ma, May i wear it now? Monu asked.
+No,My dear,the rains are near,but just now sky is clear, said Ma.`;
+
+console.log(write);
+
